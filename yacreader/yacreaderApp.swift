a9934589902d1942b0530458b148140a89af7a19
GaoJun <1920363953@qq.com>
2026-03-17 23:25:28 +0800

    Initial Commit

---
+++ yacreader/yacreaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  yacreaderApp.swift
+//  yacreader
+//
+//  Created by 欧君子 on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct yacreaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
